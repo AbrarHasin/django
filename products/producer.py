f3c3d831a5b97e1fece7a5fdc7296c9818afb4f9
abrarbjit <abrar.hasin@bjitgroup.com>
2023-07-17 17:20:32 +0600

    khub hoise!

--- products/producer.py
+++ products/producer.py
@@ -7,6 +7,6 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish():
-    # properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='admin', body="Hello")
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body))
